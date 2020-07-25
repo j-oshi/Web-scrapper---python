4ba8149ace8216b565f2ef7388c9c54fd404d033
jayosh65 <jtoshin@gmail.com>
2020-07-25 02:42:47 +0100

    add html function

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,7 @@
 import urllib.request
+from lxml import etree
+from io import StringIO
+
 
 class Scrapper:
     def __init__(self, url):
@@ -12,7 +15,7 @@ class Scrapper:
         # if request.status_code == 200:
         #     print('Web site exists')
         # else:
-        #     print('Web site does not exist') 
+        #     print('Web site does not exist')
         # conn = http.client.HTTPSConnection(self.url)
         # conn.request("HEAD", "")
         # value = True
@@ -27,13 +30,39 @@ class Scrapper:
             # still using internet explorer.
             headers = {}
             headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"
-            req = urllib.request.Request(url, headers = headers)
+            req = urllib.request.Request(url, headers=headers)
             resp = urllib.request.urlopen(req)
             respData = resp.read()
+            data = self.parse_html(respData)
 
-            saveFile = open(textFile, "wb")
-            saveFile.write(respData)
-            # saveFile.write(str(respData))
+            saveFile = open(textFile, "w")
+
+            for element in data:
+                saveFile.write(element)
+                saveFile.write('\n')
             saveFile.close()
         except Exception as e:
             print(str(e))
+
+    def parse_html(self, htmlcontent):
+        parser = etree.HTMLParser()
+        tree = etree.parse(StringIO(str(htmlcontent)), parser=parser)
+        nodes = tree.xpath("//a")
+        # Get the url from the ref
+        # links = [link.get('href', '')
+        list = []
+        for el in nodes:
+            list.append(el.get('href', ''))
+        return list
+         
+    # Read site robot.txt
+    def get_site_robot_txt(self):
+        # stream = urllib.request.urlopen(self.url + "/robots.txt")
+        stream = urllib.request.urlopen(
+            "https://jobs.sanctuary-group.co.uk/robots.txt")
+        print(stream.read().decode("utf-8"))
+
+
+scanSite = Scrapper("https://jobs.sanctuary-group.co.uk/search/")
+scanSite.get_web_content('test3.txt')
+# scanSite.get_site_robot_txt()
